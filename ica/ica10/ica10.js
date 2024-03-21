016cad257799e189e8e5f7d19247eb5fce95f125
aidanbertram <136124958+aidanbertram@users.noreply.github.com>
2024-03-20 23:08:11 -0600

    ica10

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,7 @@
+document.getElementById("btn1").addEventListener("click", function() {
+    document.getElementById("text").textContent = "Changed text";
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+    document.body.style.backgroundColor = "yellow";
+});
